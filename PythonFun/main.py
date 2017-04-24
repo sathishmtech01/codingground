2c5fdf60a59859141cbc04b9e49175677133fe85
Sathish <sathish.mtech01@gmail.com>
2017-04-24 15:57:47 +0530

    Created by Coding Ground Client

---
+++ PythonFun/main.py
@@ -0,0 +1,9 @@
+# Tricks in Python
+import tricks
+# Making for loop simple for dictonary and array if need
+print(tricks.trick01())
+# fun Zip function
+print(tricks.trick02())
+
+# fun Zip function
+print(tricks.trick03())
